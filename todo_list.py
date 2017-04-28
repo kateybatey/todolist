c58add08a6466e44a4d669789d211708fd0d9519
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-27 21:01:37 -0700

    some commit

---
+++ todo_list.py
@@ -0,0 +1,49 @@
+"""This is a Terminal-based program that allows a user to create and edit a to-do list.
+
+The stub of each function has been provided. Read the docstrings for what each 
+function should do and complete the body of the functions below.
+
+You can run the script in your Terminal at any time using the command:
+
+    >>> python todo_list.py
+
+"""
+
+def add_to_list(my_list):
+    """Takes user input and adds it as a new item to the end of the list."""
+
+    print "What would you like to add to the list?"
+    new_item = raw_input()
+    my_list.append(new_item)
+
+
+def view_list(my_list):
+    """Print each item in the list."""
+    for item in my_list:
+        print item
+
+def display_main_menu(my_list):
+    """Displays main options, takes in user input, and calls view or add function."""
+
+    user_options = """
+    \nWould you like to:
+    A. Add a new item
+    B. View list
+    C. Quit the program
+    >>> """
+
+    while True:
+        user_input = raw_input(user_options)
+        if user_input.upper() == "A":
+            add_to_list(my_list)
+        elif user_input.upper() == "B":
+            view_list(my_list)
+        elif user_input.upper() == "C":
+            break
+        else:
+            print "Not valid entry. Enter A, B or C."    
+#-------------------------------------------------
+
+my_list = []
+display_main_menu(my_list)
+
